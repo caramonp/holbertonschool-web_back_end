175f8977672b9b2a1a35fd9bfd641a7148853e6f
Carolina <carolinaramon239578@correo.itm.edu.co>
2022-01-31 18:11:32 -0500

    Variable Annotations

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""function concat that takes two string"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """returns a concatenation of two strings"""
+    return "{}{}".format(str1, str2)
