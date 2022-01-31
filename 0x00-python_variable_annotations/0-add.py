45ea37b7c044ae6dd786e86e2a6d5bdd7034c325
Carolina <carolinaramon239578@correo.itm.edu.co>
2022-01-31 17:58:35 -0500

    readme and sum

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+""" function add that takes a two floats and return sum"""
+
+
+def add(a: float, b: float) -> float:
+    """returns the sum of two floats"""
+    return a + b
