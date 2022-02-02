e3e744b63affaa2e90372c3d257ed382a91a59d6
Carolina <carolinaramon239578@correo.itm.edu.co>
2022-02-02 17:05:05 -0500

    pep8

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,11 +7,11 @@ from typing import Generator
 import random
 
 async def async_generator() -> Generator[float, None, None]:
-  """coroutine will loop 10 times, each time asynchronously wait 1 second
+    """coroutine will loop 10 times, each time asynchronously wait 1 second
 
-  Yields:
-      Generator[float, None, None]
-  """
-  for _ in range(10):
-    await asyncio.sleep(1)
-    yield (random.uniform(0, 10))
+    Yields:
+        Generator[float, None, None]
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield (random.uniform(0, 10))
