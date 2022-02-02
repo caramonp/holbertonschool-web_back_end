7f8669d321d1532fb7e635bf52b7475a9e083f6c
Carolina <carolinaramon239578@correo.itm.edu.co>
2022-02-02 17:27:11 -0500

    withput 10

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -7,7 +7,7 @@ from typing import Generator
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> Generator[float]:
+async def async_comprehension() -> Generator[float, None, None]:
     """[oroutine will collect 10 random numbers using an async comprehensing]
 
     Returns:
