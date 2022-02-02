91c4957952621b19808d43dbcaa1a45d3d833c0b
Carolina <carolinaramon239578@correo.itm.edu.co>
2022-02-02 17:03:02 -0500

    commitments

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,6 +7,11 @@ from typing import Generator
 import random
 
 async def async_generator() -> Generator[float, None, None]:
-  for i in range(10):
+  """coroutine will loop 10 times, each time asynchronously wait 1 second
+
+  Yields:
+      Generator[float, None, None]
+  """
+  for _ in range(10):
     await asyncio.sleep(1)
-    yield (random.random(0, 10))
+    yield (random.uniform(0, 10))
