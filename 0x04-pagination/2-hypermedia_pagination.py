53d8e155cabe38282727fc1f5791474c6188cb4e
Carolina <carolinaramon239578@correo.itm.edu.co>
2022-02-08 21:20:31 -0500

    hypermedia pagination

--- 0x04-pagination/2-hypermedia_pagination.py
+++ 0x04-pagination/2-hypermedia_pagination.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 """Pagination"""
+
 import csv
-from typing import Tuple, List
+import math
+from typing import List
 
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
+def index_range(page: int, page_size: int) -> tuple:
     """helper funtion"""
-    return (((page - 1) * page_size), (page * page_size))
+    return ((page - 1) * page_size, page_size * page)
 
 
 class Server:
@@ -29,6 +31,8 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """takes two integer arguments page with
+        default value 1 and page_size with default value 10"""
         assert isinstance(page, int)
         assert page > 0
         assert isinstance(page_size, int)
@@ -36,3 +40,17 @@ class Server:
 
         a, b = index_range(page, page_size)
         return self.dataset()[a:b]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """returns a dictionary
+        """
+        total = math.ceil(len(self.dataset()) / page_size)
+        dictionary = {
+            'page_size': len(self.get_page(page, page_size)),
+            'page': page,
+            'data': self.get_page(page, page_size),
+            'next_page': page + 1 if page < total else None,
+            'prev_page': page - 1 if page > 1 else None,
+            'total_pages': total
+        }
+        return dictionary
