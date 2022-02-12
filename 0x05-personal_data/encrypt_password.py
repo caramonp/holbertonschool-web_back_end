ce7d2784b832bb50aa5a11bba43361b2c3fe6407
Carolina <carolinaramon239578@correo.itm.edu.co>
2022-02-11 21:00:49 -0500

    check password

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -16,4 +16,5 @@ def hash_password(password: str) -> bytes:
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """ Check valid password
     """
-    return bcrypt.checkpw(password, hashed_password)
+    if bcrypt.checkpw(password, hashed_password)
+    return 
