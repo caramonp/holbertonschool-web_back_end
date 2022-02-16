2816249cc9e06cea7510b010d16362c198852411
Carolina <carolinaramon239578@correo.itm.edu.co>
2022-02-15 21:27:34 -0500

    pycodestyle

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -10,6 +10,7 @@ from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 import os
 
+
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
