5cc45a70fe4873f1aadbfb084ed6dbba64ea989d
Carolina <carolinaramon239578@correo.itm.edu.co>
2022-02-17 20:16:38 -0500

    session cookie

--- 0x07-Session_authentication/api/v1/auth/auth.py
+++ 0x07-Session_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 """
 from flask import request
 from typing import List, TypeVar
+from os import getenv
 
 
 class Auth:
@@ -37,3 +38,11 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         '''self descriptive'''
         return None
+
+    def session_cookie(self, request=None):
+        '''self descriptive'''
+        if request is None:
+            return None
+
+        cookie_key = getenv('SESSION_NAME')
+        return request.cookies.get(cookie_key)
