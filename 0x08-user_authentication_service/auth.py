778f364af148d242adbc5abd083ef577f197be41
Carolina <carolinaramon239578@correo.itm.edu.co>
2022-02-25 23:58:50 -0500

    pep8

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -4,7 +4,8 @@ Auth module
 """
 import hashlib
 
+
 def _hash_password(password: str) -> str:
     """hash a password pass for user
     """
-    return bcrypt.hashpw(password, bcrypt.gensalt())
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
