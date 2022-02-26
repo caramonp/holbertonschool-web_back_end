85400c84991b8b2af1e22c3ec90a37b02db88f91
Carolina <carolinaramon239578@correo.itm.edu.co>
2022-02-25 23:34:19 -0500

    pep8 style

--- 0x08-user_authentication_service/db.py
+++ 0x08-user_authentication_service/db.py
@@ -28,7 +28,6 @@ class DB:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
-    
 
     def add_user(self, email: str, hashed_password: str) -> User:
         """add user to database
