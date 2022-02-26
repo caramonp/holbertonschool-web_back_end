401c86a37d3a3ef29ef6a40a959f567dfe2bdd97
Carolina <carolinaramon239578@correo.itm.edu.co>
2022-02-25 23:56:38 -0500

    hash password

---
+++ 0x08-user_authentication_service/auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Auth module
+"""
+import hashlib
+
+def _hash_password(password: str) -> str:
+    """hash a password pass for user
+    """
+    return bcrypt.hashpw(password, bcrypt.gensalt())
