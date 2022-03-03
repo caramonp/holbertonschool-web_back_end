941ce495db1876af493b8f57fde4140239f211ad
Carolina <carolinaramon239578@correo.itm.edu.co>
2022-03-03 13:15:27 -0500

    readme file and basic Flask app

---
+++ 0x0A-i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""basic Flask app
+
+Returns:
+    HTML with Hello word
+"""
+from flask import Flask
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """ return index.html
+    """
+    return render_template('index.html')
