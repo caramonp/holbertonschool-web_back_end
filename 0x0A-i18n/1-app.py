59532338c26c60371ff0293f873d2f77a3dbeca5
Carolina <carolinaramon239578@correo.itm.edu.co>
2022-03-03 17:12:16 -0500

    Basic Babel setup

---
+++ 0x0A-i18n/1-app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""basic Babel setup
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config(object):
+    """ Class to config available languages
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+        
+
+app = Flask(__name__)
+app.config.from_objec(Config)
+babel = Babel(app)
+
+@app.route('/')
+def index():
+    """ return index.html
+    """
+    return render_template('1-index.html')
