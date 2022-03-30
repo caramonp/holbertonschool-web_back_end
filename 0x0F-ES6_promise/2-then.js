7b56e211e01d231a772e2c5f5b61b8dd05e26938
Carolina <carolinaramon239578@correo.itm.edu.co>
2022-03-30 16:14:46 -0500

    use of method finally

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,11 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-    return promise.then(() => (
-        {
-            status: 200,
-            body: 'Success',
-        }
-    )).catch(() => (new Error('The fake API is not working currently')))
-    .then(() => (
-      console.log('Got a response from the API')
-    ));
+  return promise.then(() => (
+    {
+      status: 200,
+      body: 'Success',
+    }
+  )).catch(() => Error()).finally(() => console.warn('Got a response from the API'));
 }
