dff2bb3b568efdc746464cbc02ef577b73a7b440
Carolina <carolinaramon239578@correo.itm.edu.co>
2022-03-30 22:07:15 -0500

    eslint changes

--- 0x0F-ES6_promise/100-await.js
+++ 0x0F-ES6_promise/100-await.js
@@ -11,7 +11,5 @@ export default async function asyncUploadUser() {
   return Promise.all([uploadPhoto(), createUser()]).then((values) => ({
     photo: values[0],
     user: values[1],
-  })).catch(() => ({
-    createObject,
-  }));
+  })).catch(() => createObject());
 }
