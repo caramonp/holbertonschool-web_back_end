7a21ce6c288cb66b4660118830af80451cd6a953
Carolina <carolinaramon239578@correo.itm.edu.co>
2022-04-07 23:21:52 -0500

    trate to fix

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,11 @@
 export default function cleanSet(theSet, startString) {
   const newArray = [];
-  if (startString) {
+  if (typeof (startString) === 'string' && startString ) {
     theSet.forEach((element) => {
       if (element.startsWith(startString)) {
         newArray.push(element.replace('bon', ''));
       }
     });
-  } else if (typeof (startString) !== 'string') {
-    return ('');
   } else {
     return ('');
   }
