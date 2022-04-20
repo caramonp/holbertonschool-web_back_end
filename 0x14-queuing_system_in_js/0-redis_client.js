2858bac2af955f34e4870c4406a599113da71989
Carolina <carolinaramon239578@correo.itm.edu.co>
2022-04-19 22:20:47 -0500

    connect to the Redis server

---
+++ 0x14-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,10 @@
+import { createClient } from "redis";
+
+const client = createClient();
+client.on("error", (error) => {
+  console.log(`Redis client not connected to the server: ${error}`);
+});
+
+client.on("ready", () => {
+  console.log("Redis client connected to the server");
+});
